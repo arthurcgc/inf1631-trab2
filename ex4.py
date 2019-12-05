8a972684a002ed6618923278b991ae8235d3b476
Arthur Coelho <arthur.cavalcante.puc@gmail.com>
2019-12-05 15:34:06 -0300

    ex4 -> product

--- ex4.py
+++ ex4.py
@@ -16,5 +16,18 @@ def max_subarray(arr):
     print(lookupv2)
     return max(lookup)
 
+def maxProduct(self, nums: List[int]):
+        global_max = nums[0]
+        imax, imin = nums[0],nums[0]
+        
+        for i in range(1,len(nums)):
+            print(local_min, local_max, global_max)
+            candidates = (nums[i], imax * nums[i], imin * nums[i])
+            imax = max(candidates)
+            imin = min(candidates)            
+            global_max = max(global_max, imax)
+        return global_max
+
 if __name__ == "__main__":
     print(max_subarray([1,2]))
+
